87f7e9ea188d1d8b7a6f1b2ffb0efe2e71a2d193
Hector Toro <alejotoro94@hotmail.com>
2023-04-30 15:23:48 -0500

    se sube la homework de filtrado por order y gender

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,16 +1,40 @@
 import { connect } from "react-redux";
 import Card from "../Card/Card";
+import { filterCards, orderCard } from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 const Favorite = (props) => {
     const {myFavorites} = props;
+    const [ aux, setAux ] = useState(false);
+    const dispatch = useDispatch();
+
+    const handleOrder = (e) => {
+        dispatch(orderCard(e.target.value));
+        setAux(!aux);
+    }
+    const handleFilter = (e) => {
+        dispatch(filterCards(e.target.value));
+    }
+
     return (
         <div>
             <h2>My favorites</h2>
+            <select onChange={handleOrder}>
+                <option value="A">Ascendete</option>
+                <option value="D">Descendente</option>
+            </select>
+            <select onChange={handleFilter}>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">unknown</option>
+            </select>
             {
-                myFavorites?.map( personaje => {
+                myFavorites?.map( (personaje,index) => {
                     return (
                         <Card  onClose={personaje.onClose}
-                        key={personaje.id} 
+                        key={index} 
                         id={personaje.id}
                         name={personaje.name} 
                         status={personaje.status}
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,45 @@
-import { ADD_FAV, REMOVE_FAV } from "./actions-type";
+import { ADD_FAV, REMOVE_FAV, FITLER, ORDER } from "./actions-type";
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 const reducer = (state = initialState,{type,payload}) => {
     switch (type) {
         case ADD_FAV:
+            const copyAllCharacters = [...state.allCharacters, payload];
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, payload]
+                myFavorites: copyAllCharacters,
+                allCharacters: [...copyAllCharacters]
             }
         case REMOVE_FAV:
             return {
                 ...state,
                 myFavorites: state.myFavorites.filter( personaje => personaje.id !== Number(payload))
+            }
+        case FITLER:
+            const copyAllCharactersFilter = state.allCharacters.filter( personaje => personaje.gender === payload)
+            return {
+                ...state,
+                myFavorites: copyAllCharactersFilter
+            }
+        case ORDER:
+            const order = state.allCharacters.sort( (a,b) => {
+                if(payload === 'A') {
+                    if(a.id < b.id) return -1;
+                    if(b.id < a.id) return 1;
+                    return 0
+                } else {
+                    if(a.id < b.id) return 1;
+                    if(b.id < a.id) return -1;
+                    return 0
+                }
+            })
+            return {
+                ...state,
+                myFavorites: order
             }
         default:
             return {...state}
